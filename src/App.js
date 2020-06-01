cdfeac2a3fe8dbc7be063c50ed2710497ed1ff69
Santiago Chopitea <elchopi26@gmail.com>
2020-06-01 01:28:51 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import Formulario from './componentes/Formulario';
+import Cancion from './componentes/Cancion';
+import Info from './componentes/Info';
+
 
 function App() {
+
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
+
+  useEffect(() => {
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    
+    const callAPILetra = async () => {
+      const {artista, cancion} = busquedaletra;
+      const urlLetra = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const urlInformaBanda = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      const [letracancion, informacion] = await Promise.all([
+        axios.get(urlLetra),
+        axios.get(urlInformaBanda)
+      ]);
+
+      guardarLetra(letracancion.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
+
+      // CON ESTA LINEA EVITAMOS LOOP
+      guardarBusquedaLetra({});
+    }
+    callAPILetra();
+  }, [busquedaletra, info]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <Formulario
+          guardarBusquedaLetra={guardarBusquedaLetra}
+        />
+
+        <div className='container mt-5'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <Info
+                info={info}
+              />
+            </div>
+            <div className='col-md-6'>
+              <Cancion
+                letra={letra}
+              />
+            </div>
+          </div>
+        </div>
+    </Fragment>
   );
 }
 
